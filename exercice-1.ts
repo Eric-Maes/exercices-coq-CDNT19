19922e4c8c473e0168c84ca4cba5b5831b288b11
Eric-Ms <ericmaes.github@gmail.com>
2022-11-28 22:26:13 +0100

    Update exercises

--- exercice-1.ts
+++ exercice-1.ts
@@ -1,14 +1,14 @@
 /**
  * Mon objet chat
  */
-interface Chat {
+ interface Chat {
   // Attributs
   nom: string;
   race: string;
   age: number;
   poids: number;
 }
-let chat = {
+let monChat = {
   nom: "Zack",
   race: "Maine Coon",
   age: 3,
@@ -21,11 +21,11 @@ let chat = {
  * @param {String[]} surnoms 
  * @returns {String}
  */
-function chatDetails(chat: Chat, surnoms: string[]): string {
-  return `Informations concernant mon chat:\n-Nom: ${chat.nom}\n-Race: ${chat.race}\n-Âge: ${chat.age} ans\n-Poids: ${chat.poids} Kg\n\n-Surnoms: ${surnoms}\n`;
+function chatDetails(monChat: Chat, surnoms: string[]): string {
+  return `Informations concernant mon chat:\n-Nom: ${monChat.nom}\n-Race: ${monChat.race}\n-Âge: ${monChat.age} ans\n-Poids: ${monChat.poids} Kg\n\n-Surnoms: ${surnoms}\n`;
 }
 
 /**
  * Log dans la console de la fonction chatDetails() accompagné de la liste des surnoms
  */
-console.log(chatDetails(chat,["Zackinou", "Sozack", "Garfield (parce que c'est un gros sac)"]));
+console.log(chatDetails(monChat,["Zackinou", "Sozack", "Garfield (parce que c'est un gros sac)"]));
---
+++ exercice-2.ts
@@ -0,0 +1,6 @@
+function wtf() {
+  return `From that moment, I understood absolutelyyyy nothing. o_O`;
+}
+
+console.log(wtf());
+
